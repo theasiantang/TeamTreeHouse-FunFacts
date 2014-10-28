261674dcc56e56968d1dda31d0e444a8860b2936
Kevin Tang <theasiantang@users.noreply.github.com>
2014-10-28 12:12:20 +0000

    Fun Facts Android App
    Finished part 3 of the course

---
+++ FunFacts/app/src/main/java/kevintang/funfacts/FunFactsActivity.java
@@ -0,0 +1,83 @@
+package kevintang.funfacts;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
+
+
+public class FunFactsActivity extends Activity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fun_facts);
+
+        // Declare are view variables and assign the Views from the layout file
+        final TextView factLabel = (TextView)findViewById(R.id.factText);
+        Button showFactButton = (Button)findViewById(R.id.showFactButton);
+
+        View.OnClickListener listener = new View.OnClickListener()
+        {
+          @Override
+          public void onClick(View view)
+          {
+            String[] facts =
+            {
+                    "Ants stretch when they wake up in the morning.",
+                    "Ostriches can run faster than horses.",
+                    "Olympic gold medals are actually made mostly of silver.",
+                    "You are born with 300 bones; by the time you are an adult you will have 206.",
+                    "It takes about 8 minutes for light from the Sun to reach Earth.",
+                    "Some bamboo plants can grow almost a meter in just one day.",
+                    "The state of Florida is bigger than England.",
+                    "Some penguins can leap 2-3 meters out of the water.",
+                    "On average, it takes 66 days to form a new habit.",
+                    "Mammoths still walked the earth when the Great Pyramid was being built."
+            };
+
+            // Button was clicked, so update the fact label with a new fact
+            String fact = "";
+            // Randomly select a fact
+            Random randomGenerator = new Random(); // Construct a new random number generator
+            int randomNumber = randomGenerator.nextInt(facts.length);
+
+            fact = facts[randomNumber];
+
+            // Update the label with our dynamic fact
+            factLabel.setText(fact);
+          }
+        };
+
+        showFactButton.setOnClickListener(listener);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.fun_facts, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
